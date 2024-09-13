01d547b060207eacdc7b01cd505ac72796428372
Deepak Chaurasiya <deepak.chaurasiya@nmgtechnologies.com>
2024-09-13 20:05:02 +0530

    Routing metada - static and dynamic (generateMetadata)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: 'About page',
+  description: 'This is about page'
+}
 export default function AboutPage() {
   return <div>About Page</div>;
 }
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,11 +1,36 @@
 // Route: "/products/<productId>"
-export default function ProductDetailsPage({
-  params,
-}: {
+
+import { Metadata } from "next";
+
+type ProductDetailsPageProps = {
   params: {
     productId: string;
   };
-}) {
+};
+
+// export const generateMetadata = ({
+//   params,
+// }: ProductDetailsPageProps): Metadata => {
+//   return {
+//     title: `Product ${params.productId}`,
+//   };
+// };
+
+export const generateMetadata = async ({
+  params,
+}: ProductDetailsPageProps): Promise<Metadata> => {
+  const title: string = await new Promise((resolve) =>
+    setTimeout(() => {
+      resolve(`Product ${params.productId}`);
+    }, 2000)
+  );
+  return {
+    title,
+  };
+};
+export default function ProductDetailsPage({
+  params,
+}: ProductDetailsPageProps) {
   return (
     <div>
       <h3>Product Details - {params.productId}</h3>
